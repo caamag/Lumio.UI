533ec07aa6631d350028c8d489f24b082cadc6c9
unknown <caiolopesfv@gmail.com>
2026-03-02 12:23:00 -0300

    Removendo playground

--- playground/App.tsx
+++ playground/App.tsx
@@ -1,27 +1,5 @@
-import Router from "./router";
-import { createGlobalStyle } from "styled-components";
-
-const GlobalStyle = createGlobalStyle`
-    * {
-        padding: 0px;
-        margin: 0px;
-        border: none;
-    }
-
-    html {
-        scroll-behavior: smooth;
-        font-size: 62.5%;
-        font-family: 'Inter', sans-serif;
-    }
-`;
-
 const App = () => {
-  return (
-    <>
-      <GlobalStyle />
-      <Router />
-    </>
-  );
+  return <>Hello!!</>;
 };
 
 export default App;
