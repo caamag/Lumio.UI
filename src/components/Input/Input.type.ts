1fc921172902a48f84d38356b77d97b968cc8d9b
unknown <caiolopesfv@gmail.com>
2026-03-05 21:17:19 -0300

    Removendo obrigatoriedade da variant do input

--- src/components/Input/Input.type.ts
+++ src/components/Input/Input.type.ts
@@ -1,7 +1,7 @@
 import { type InputHTMLAttributes, type JSX } from "react";
 
 export interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
-  variant: "normal" | "underlined" | "dotted";
+  variant?: "normal" | "underlined" | "dotted";
   width?: string;
   hasIcon?: boolean;
   icon?: string | JSX.Element;
